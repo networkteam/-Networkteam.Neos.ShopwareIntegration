3875aec282e06850cacb895f196d56a33caf3578
Sebastian Flor <sebastian.flor@networkteam.com>
2020-08-14 16:21:58 +0200

    Restructure cookie handling

--- Resources/Private/Javascript/Connector.js
+++ Resources/Private/Javascript/Connector.js
@@ -2,6 +2,7 @@ import 'core-js';
 import 'regenerator-runtime/runtime';
 import axios from 'axios';
 
+import { getCookie, setCookie } from './Helper/cookieHelper';
 import * as endpoints from './Api/endpoints';
 
 export class ShopwareConnector {
@@ -11,51 +12,50 @@ export class ShopwareConnector {
     this.client = axios.create({
       baseURL: baseUri,
       headers: {
-        'sw-access-key': apiKey
-      }
+        'sw-access-key': apiKey,
+      },
     });
+  }
+
+  _setContextTokenHeaderFromCookie() {
+    const currentHeader = this.client.defaults.headers['sw-context-token'];
+    const contextToken = getCookie('sw-context-token');
+    if (currentHeader && currentHeader === contextToken) {
+      return true;
+    }
+
+    if (contextToken) {
+      this.client.defaults.headers['sw-context-token'] = contextToken;
+      return true;
+    }
+
+    return;
+  }
+
+  setContextTokenHeader(newContextToken) {
+    if (newContextToken) {
+      setCookie('sw-context-token', newContextToken);
+      this.client.defaults.headers['sw-context-token'] = newContextToken;
+      return true;
+    } else {
+      return this._setContextTokenHeaderFromCookie();
+    }
   }
 
   addProductToCart(productId) {
     const url = endpoints.ADD_TO_CART_BASE + productId;
-    const contextToken = this.getCookie('sw-context-token');
-
-    if(contextToken) {
-      this.client.defaults.headers = {
-        ...this.client.defaults.headers,
-        ...{
-          'sw-context-token': contextToken
-        }
-      }
-    }
+    const hasCookie = this.setContextTokenHeader();
 
     return new Promise((resolve, reject) => {
       this.client
         .post(url)
-        .then((data) => {
-          console.log(data);
-          if(!contextToken) {
-            this.setCookie('sw-context-token', data.data.data.token);
+        .then(({ data }) => {
+          if (!hasCookie) {
+            this.setContextTokenHeader(data.data.token);
           }
           resolve('success');
         })
         .catch((e) => reject(e));
     });
-  }
-
-  setCookie(name, value, days = 7, path = '/') {
-    const expires = new Date(Date.now() + days * 864e5).toUTCString()
-    document.cookie = name + '=' + encodeURIComponent(value) + '; expires=' + expires + '; path=' + path
-  }
-
-  getCookie(name) {
-    return document.cookie.split('; ').reduce((r, v) => {
-      const parts = v.split('=')
-      return parts[0] === name ? decodeURIComponent(parts[1]) : r
-    }, '')
-  }
-
-  deleteCookie(name, path) {
-    this.setCookie(name, '', -1, path)
   }
 }
