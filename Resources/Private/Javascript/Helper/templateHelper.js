72667b2f3a28e5aadb3d3e4fefe0cd40dd5b735d
pschmidt <philip.schmidt@networkteam.com>
2020-08-16 19:58:15 +0200

    move nunjucks configuration outside of replaceTemplatePlaceholder function

--- Resources/Private/Javascript/Helper/templateHelper.js
+++ Resources/Private/Javascript/Helper/templateHelper.js
@@ -1,25 +1,27 @@
 import nunjucks from 'nunjucks';
 
+const env = nunjucks.configure({
+    tags: {
+        blockStart: '[%',
+        blockEnd: '%]',
+        variableStart: '[[',
+        variableEnd: ']]',
+        commentStart: '[#',
+        commentEnd: '#]'
+    }
+});
+
+env.addFilter('currency', (number, locales = 'de-DE', currency = 'EUR') => {
+    if(!isNaN(parseFloat(number)) && isFinite(number)) {
+        return new Intl.NumberFormat(locales, {
+            style: 'currency',
+            currency: currency
+        }).format(parseFloat(number))
+    } else {
+        return number
+    }
+});
+
 export const replaceTemplatePlaceholder = (template, data) => {
-    const env = nunjucks.configure({
-        tags: {
-            blockStart: '[%',
-            blockEnd: '%]',
-            variableStart: '[[',
-            variableEnd: ']]',
-            commentStart: '[#',
-            commentEnd: '#]'
-        }
-    });
-    env.addFilter('currency', (number, locales = 'de-DE', currency = 'EUR') => {
-        if(!isNaN(parseFloat(number)) && isFinite(number)) {
-            return new Intl.NumberFormat(locales, {
-                style: 'currency',
-                currency: currency
-            }).format(parseFloat(number))
-        } else {
-            return number
-        }
-    });
     return env.renderString(template, data);
 }
